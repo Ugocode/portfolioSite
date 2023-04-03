839629815d276139ea17d53af1bb28c87d146a18
ugocode <obiohaugo5@gmail.com>
2023-04-03 22:08:43 +0100

    modified

--- lib/Screens/mobile_projects.dart
+++ lib/Screens/mobile_projects.dart
@@ -8,64 +8,93 @@ class MobileProjects extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(title: const Text("Mobile App Projects")),
-      body: ListView(
-        children: [
-          Container(
-            height: 70,
-            width: 200,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10), color: Colors.purple),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  ProjectWidget(
-                      title: 'Sifian Website', bgColor: Colors.orange),
-                  ProjectWidget(
-                      title: 'Sifian Website', bgColor: Colors.orange),
-                  ProjectWidget(
-                      title: 'Sifian Website', bgColor: Colors.orange),
-                  ProjectWidget(
-                      title: 'Sifian Website', bgColor: Colors.orange),
-                ],
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          // second row of cards
-          Center(
-            child: Column(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('Assets/images/dark.png'), fit: BoxFit.cover),
+        ),
+        child: ListView(
+          children: [
+            // Container(
+            //   height: 70,
+            //   width: 200,
+            //   decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(10),
+            //       color: Colors.purple),
+            // ),
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                const SizedBox(
+                  height: 40,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
+                  children: [
                     ProjectWidget(
-                        title: 'Sifian Website', bgColor: Colors.orange),
+                      title: 'MoneyVest App',
+                      bgColor: Colors.orange,
+                      image: Image.asset(
+                        'Assets/images/moneyvest.PNG',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                     ProjectWidget(
-                        title: 'Sifian Website', bgColor: Colors.orange),
+                      title: 'MCare App',
+                      bgColor: Colors.orange,
+                      image: Image.asset(
+                        'Assets/images/mCare.png',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                     ProjectWidget(
-                        title: 'Sifian Website', bgColor: Colors.orange),
+                      title: 'Corona Virus App',
+                      bgColor: Colors.orange,
+                      image: Image.asset(
+                        'Assets/images/covidapp.png',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                     ProjectWidget(
-                        title: 'Sifian Website', bgColor: Colors.orange),
+                      title: 'Task App',
+                      bgColor: Colors.orange,
+                      image: Image.asset(
+                        'Assets/images/task.jpeg',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                   ],
                 )
               ],
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 40,
+            ),
+            // second row of cards
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      ProjectWidget(
+                          title: 'Sifian Website', bgColor: Colors.orange),
+                      ProjectWidget(
+                          title: 'Sifian Website', bgColor: Colors.orange),
+                      ProjectWidget(
+                          title: 'Sifian Website', bgColor: Colors.orange),
+                      ProjectWidget(
+                          title: 'Sifian Website', bgColor: Colors.orange),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
-    ;
   }
 }
--- lib/Screens/web_projects.dart
+++ lib/Screens/web_projects.dart
@@ -7,73 +7,95 @@ class WebProjects extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(title: const Text("Web Desgin Projects")),
-      body: ListView(
-        children: [
-          // Container(
-          //   height: 70,
-          //   width: 200,
-          //   decoration: BoxDecoration(
-          //       borderRadius: BorderRadius.circular(10), color: Colors.purple),
-          // ),
-
-          const SizedBox(
-            height: 40,
-          ),
-          Center(
-            child: Text(
-              "Say Goodbye to Boring and aweful looking websites and say Hello to Great engaging and mindblowing new ones.\n Having a quality and dynamic website will make your clients trust your business more and give you more revenue.\n reason why I always take my time to create the best for my clients.",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20, color: Colors.blueGrey[300]),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('Assets/images/dark.png'), fit: BoxFit.cover),
+        ),
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 40,
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 40,
+            Center(
+              child: Text(
+                "Say Goodbye to Boring and aweful looking websites and say Hello to Great engaging and mindblowing new ones.\n Having a quality and dynamic website will make your clients trust your business more and give you more revenue.\n reason why I always take my time to create the best for my clients.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, color: Colors.blueGrey[300]),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  ProjectWidget(
-                      title: 'Sifian Website', bgColor: Colors.blueGrey),
-                  ProjectWidget(
-                      title: 'Sifian Website', bgColor: Colors.blueGrey),
-                  ProjectWidget(
-                      title: 'Sifian Website', bgColor: Colors.blueGrey),
-                  ProjectWidget(
-                      title: 'Sifian Website', bgColor: Colors.blueGrey),
-                ],
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          // second row of cards
-          Center(
-            child: Column(
+            ),
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                const SizedBox(
+                  height: 40,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
+                  children: [
                     ProjectWidget(
-                        title: 'Sifian Website', bgColor: Colors.orange),
+                      title: 'Instant Website',
+                      bgColor: Colors.blueGrey,
+                      image: Image.asset(
+                        'Assets/images/instant11.PNG',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                     ProjectWidget(
-                        title: 'Sifian Website', bgColor: Colors.orange),
+                      title: 'Sifian Website',
+                      bgColor: Colors.blueGrey,
+                      image: Image.asset(
+                        'Assets/images/cifian.PNG',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                     ProjectWidget(
-                        title: 'Sifian Website', bgColor: Colors.orange),
+                      title: 'Gslm Website',
+                      bgColor: Colors.blueGrey,
+                      image: Image.asset(
+                        'Assets/images/gslm.PNG',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                     ProjectWidget(
-                        title: 'Sifian Website', bgColor: Colors.orange),
+                      title: 'Sifian Website',
+                      bgColor: Colors.blueGrey,
+                      image: Image.asset(
+                        'Assets/images/cifian.PNG',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                   ],
                 )
               ],
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 40,
+            ),
+            // second row of cards
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      ProjectWidget(
+                          title: 'Sifian Website', bgColor: Colors.orange),
+                      ProjectWidget(
+                          title: 'Sifian Website', bgColor: Colors.orange),
+                      ProjectWidget(
+                          title: 'Sifian Website', bgColor: Colors.orange),
+                      ProjectWidget(
+                          title: 'Sifian Website', bgColor: Colors.orange),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
