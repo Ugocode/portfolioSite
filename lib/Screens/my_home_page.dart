f6e7743e8bea2ce600f8f40a662851eff8093599
ugocode <obiohaugo5@gmail.com>
2023-04-21 20:22:34 +0100

    added somthing

--- lib/Screens/my_home_page.dart
+++ lib/Screens/my_home_page.dart
@@ -736,7 +736,7 @@ class MyHomePage extends StatelessWidget {
                           fit: BoxFit.fill,
                         ),
                       ),
-                    ).animate().shake(duration: const Duration(seconds: 5)),
+                    ).animate().slide(),
                   ],
                 ),
               ],
