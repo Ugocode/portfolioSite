c8257c61c44991bc6a914c2291c9e05fe406dc3e
ugocode <obiohaugo5@gmail.com>
2024-12-09 23:10:39 +0100

    added a new pics

--- lib/Screens/my_home_page.dart
+++ lib/Screens/my_home_page.dart
@@ -186,7 +186,7 @@ class MyHomePage extends StatelessWidget {
                               child: ClipRRect(
                                 borderRadius: BorderRadius.circular(10),
                                 child: Image.asset(
-                                  'Assets/images/ugoa.jpg',
+                                  'Assets/images/ugoa.jpeg',
                                   fit: BoxFit.cover,
                                 ),
                               ),
